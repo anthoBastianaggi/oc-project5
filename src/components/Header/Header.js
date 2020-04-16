3f5b95fa646042cd3f40fce47845549785f905b6
anthoBastianaggi <a.bastianaggi@gmail.com>
2020-04-16 17:56:39 +0200

    [PO-1] (Add) Create structure project portfolio

---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav class="navbar navbar-b navbar-expand-md fixed-top navbar-trans" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll" href="#page-top">BASTIANAGGI Anthony</a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll active" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#competences">Compétences</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#service">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#portfolio">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  );
+}
+
+export default Header;
---
+++ src/views/Portfolio/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../../components/Header/Header";
+
+function Portfolio() {
+  return (
+    <div>
+        <Header />
+        <div>
+            Bonjour les gens
+        </div>
+    </div>
+  );
+}
+
+export default Portfolio;
